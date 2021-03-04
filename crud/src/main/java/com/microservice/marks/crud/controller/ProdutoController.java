758a11656eed99628c6a4d08efa9902da2121530
Marks Machado de Lima Rezende <marksmlr.dev@gmail.com>
2021-03-04 20:28:51 -0300

    Contexto do controller precisa da barra no nome exposto para uso no LinkTo, do Hateoas.

--- crud/src/main/java/com/microservice/marks/crud/controller/ProdutoController.java
+++ crud/src/main/java/com/microservice/marks/crud/controller/ProdutoController.java
@@ -19,7 +19,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("produto")
+@RequestMapping("/produto")
 public class ProdutoController {
 
     private final ProdutoService produtoService;
