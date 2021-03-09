cfa935b8b966c0a181b2a723ab62e157487a84b4
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-03-09 18:00:07 -0300

    Configuracao comunicacao RabbitMQ na Exchange. Implemento no ProdutoService para enviar sinal ao rabbitmq.

---
+++ crud/src/main/java/com/microservice/marks/crud/message/ProdutoSendMessage.java
@@ -0,0 +1,32 @@
+package com.microservice.marks.crud.message;
+
+import com.microservice.marks.crud.data.vo.ProdutoVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoSendMessage {
+    private final Logger log = LoggerFactory.getLogger(ProdutoSendMessage.class);
+
+    @Value("${crud.rabbitmq.exchange}")
+    String exchange;
+
+    @Value("${crud.rabbitmq.routingkey}")
+    String routingkey;
+
+    public final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public ProdutoSendMessage(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(ProdutoVO produtoVO) {
+        log.info("Mensagem sendo enviada para Exchange '".concat(exchange).concat("' Routingkey '").concat(routingkey).concat("'"));
+        rabbitTemplate.convertAndSend(exchange, routingkey, produtoVO);
+    }
+}
--- crud/src/main/java/com/microservice/marks/crud/service/ProdutoService.java
+++ crud/src/main/java/com/microservice/marks/crud/service/ProdutoService.java
@@ -3,6 +3,7 @@ package com.microservice.marks.crud.service;
 import com.microservice.marks.crud.data.vo.ProdutoVO;
 import com.microservice.marks.crud.entity.Produto;
 import com.microservice.marks.crud.exception.ResourceNotFoundException;
+import com.microservice.marks.crud.message.ProdutoSendMessage;
 import com.microservice.marks.crud.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,14 +16,18 @@ import java.util.Optional;
 public class ProdutoService {
     private static final String MESAGE_NOT_FOUND_ID = "No record found for this ID";
     private final ProdutoRepository produtoRepository;
+    private ProdutoSendMessage produtoSendMessage;
 
     @Autowired
-    public ProdutoService(ProdutoRepository produtoRepository) {
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoSendMessage produtoSendMessage) {
         this.produtoRepository = produtoRepository;
+        this.produtoSendMessage = produtoSendMessage;
     }
 
     public ProdutoVO create(ProdutoVO produtoVO) {
-        return ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+        var entityVO = ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+        produtoSendMessage.sendMessage(entityVO);
+        return entityVO;
     }
 
     public Page<ProdutoVO> findAll(Pageable pageable) {
