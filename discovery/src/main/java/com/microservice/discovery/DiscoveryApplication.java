3b457529e64d99f5263ab31c201ca0dffa5ca8c2
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-03-28 20:51:49 -0300

    Criado projeto Discovery, com o papel de Serive Registry com Eureka (Netflix)

---
+++ discovery/src/main/java/com/microservice/discovery/DiscoveryApplication.java
@@ -0,0 +1,23 @@
+package com.microservice.discovery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+	private static final Logger log = LoggerFactory.getLogger(DiscoveryApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+		logRunnerInitial();
+	}
+
+	private static void logRunnerInitial() {
+		log.info("MICROSERVICE AUTH IS RUNNING");
+	}
+
+}
