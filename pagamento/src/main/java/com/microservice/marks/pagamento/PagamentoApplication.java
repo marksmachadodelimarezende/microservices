65829accf09501ba0737e6b0572fcde85ff7435a
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-03-28 20:52:23 -0300

    Ajuste projeto microservico pagamento para registrar no discovery

--- pagamento/src/main/java/com/microservice/marks/pagamento/PagamentoApplication.java
+++ pagamento/src/main/java/com/microservice/marks/pagamento/PagamentoApplication.java
@@ -3,9 +3,11 @@ package com.microservice.marks.pagamento;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.microservice")
+@EnableDiscoveryClient
 public class PagamentoApplication {
 
     public static void main(String[] args) {
